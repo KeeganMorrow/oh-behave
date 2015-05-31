54413db66f28b686b97075326434c54c940d22dc
Korrow <Korrow@gmail.com>
2015-05-31 01:29:15 -0700

    Several tweaks addition to behavior
    Add wrapper functions to node methods to contain common code. At
    present this is mostly logging, but could be useful for other stuff
    later on.

--- mod/behave.py
+++ mod/behave.py
@@ -5,6 +5,18 @@ import enum
 
 ERR_ABSTRACT_CALL = "Attempted to call abstract method"
 
+def print_node_decorator(func):
+
+    def func_decorator(*args, **kwargs):
+        msg = '{0} running'.format(func.__name__)
+        ret = func(*args, **kwargs)
+        if ret in ExecuteResult:
+            msg += ' returns status \"{0}\"'.format(str(ret))
+        print(msg)
+        return ret
+
+    return func_decorator
+
 class ExecuteResult(enum.Enum):
     """
     Represents results of executing a node
@@ -20,23 +32,47 @@ class Node:
     def __init__(self):
         pass
 
-    def success(self):
-        """
-        Abstract method to run on node completion
-        """
-        raise NotImplementedError(ERR_ABSTRACT_CALL)
-
+    @print_node_decorator
     def execute(self):
         """
-        Abstract method for executing the node
+        Wrapper with some common code for execution of nodes
+        """
+        ret = self._execute()
+        return ret
+
+    @print_node_decorator
+    def failed(self):
+        """
+        Wrapper with some common code for execution of nodes
+        """
+        ret = self._failed()
+        return ret
+
+    @print_node_decorator
+    def success(self):
+        """
+        Wrapper with some common code for success methods of nodes
+        """
+        ret = self._success()
+        return ret
+
+    def _execute(self):
+        """
+        Abstract private method for executing the node
 
         Implementations should return an ExecuteResult
         """
         raise NotImplementedError(ERR_ABSTRACT_CALL)
 
-    def failed(self):
+    def _failed(self):
         """
         Abstract method to run on node failure
+        """
+        raise NotImplementedError(ERR_ABSTRACT_CALL)
+
+    def _success(self):
+        """
+        Abstract method to run on node success
         """
         raise NotImplementedError(ERR_ABSTRACT_CALL)
 
@@ -47,6 +83,11 @@ class NodeComposite(Node):
         self._children = []
 
     def addchild(self, childnode):
+        """
+        Wrapper with some common code for addchild methods of composite nodes
+        """
+        return self._addchild(childnode)
+    def _addchild(self, childnode):
         """ Add a child to the composite node"""
         self._children.append(childnode)
 
@@ -55,51 +96,32 @@ class NodeSequence(NodeComposite):
 
     def __init__(self):
         super().__init__()
+        self.name = 'sequence'
 
-    def success(self):
+    def _success(self):
         pass
 
-    def failed(self):
+    def _failed(self):
         pass
 
-    def execute(self):
+    def _execute(self):
         """Execute the child nodes in a sequence"""
+
         if len(self._children) == 0:
-            return ExecuteResult.success
+            status = ExecuteResult.success
+        else:
+            firstnode = self._children[0]
+            status = firstnode.execute()
+            if status is ExecuteResult.success:
+                firstnode.success()
+                self._children.pop(0)
+                if len(self._children) == 0:
+                    status = ExecuteResult.success
+                else:
+                    status = ExecuteResult.ready
 
-        firstnode = self._children[0]
-        status = firstnode.execute()
-        if status is ExecuteResult.success:
-            firstnode.success()
-            self._children.pop(0)
-            if len(self._children) == 0:
-                status =  ExecuteResult.success
-            else:
-                status =  ExecuteResult.ready
+            elif status is ExecuteResult.failure:
+                firstnode.failed()
 
-        elif status is ExecuteResult.failure:
-            firstnode.failed()
         return status
-
-class NodePrintLeaf(Node):
-    """
-    Simple leaf for initial testing purposes
-    """
-    def __init__(self, executemsg, failmsg, completemsg, status):
-        super().__init__()
-        self.executemsg = executemsg
-        self.failmsg = failmsg
-        self.completemsg = completemsg
-        self.status = status
-
-    def success(self):
-        print(self.completemsg)
-
-    def failed(self):
-        print(self.failmsg)
-
-    def execute(self):
-        print(self.executemsg)
-        return self.status
-
 
